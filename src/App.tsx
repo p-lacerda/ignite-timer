42264b3316acf39cf7b663733af56f5d157766da
Paulo de Tasso <xcloudgameemail@gmail.com>
2022-08-24 08:03:38 -0300

    feat: add routes, theme provider and global style to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import Router from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Router />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
